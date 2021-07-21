d80948a202fecfdaa454f79613899360695112a3
Mirris <miroslavhons@gmail.com>
2021-07-21 23:46:20 +0200

    hash function 1.0

--- src/main.py
+++ src/main.py
@@ -1,1 +1,21 @@
-print('Hello World!')
+import hashlib
+
+
+# sample encryption function showing condensed usage of SHA 256 encryption algorythm
+def sha_256_encrypt(my_string):
+
+    # encode() function used rather than update() function for lower-level string conversion to the byte-stream
+    encoded_string = my_string.encode()
+
+    # sha256() constructor selected to create SHA-256 hash object
+    encrypted_string = hashlib.sha256(encoded_string)
+
+    # hexdigest() function used rather than digest() function to assure safe hash transfer via non-binary environments
+    safe_sha256 = encrypted_string.hexdigest()
+
+    return safe_sha256
+
+
+string_sample = input("Input text string for SHA 256 encryption: ")
+sha_signature = sha_256_encrypt(string_sample)
+print("Encrypted output: " + sha_signature)
